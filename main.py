75483b7e66973783075b4c1b5018a5827c84ffca
Egor Chernyshev <egorch.formal@gmail.com>
2021-10-29 15:01:07 +0100

    Implemented angle calculation function in utilities class

--- main.py
+++ main.py
@@ -1,24 +1,57 @@
 # Copyright - Egor Chernyshev. SpaceShootaz - game made for University of Manchester python coursework
 # DO NOT RESIZE THE WINDOW. Window initializes with correct size at start
 import tkinter as tk
+import math
 
 
-class Utilities: # Class for utility functions, such as resolve angle and resolve point
+class Utilities:
+  # Class for utility functions, such as resolve angle and resolve point
+  def __init__(self):
+    pass
+
+  def radians_to_degrees(self, radians: float) -> float:
+    return radians * (180 / math.pi)
+
+  def resolve_angle(self, x1: float, y1: float, x2: float, y2:float ) -> float:
+    # Calculates angle between (x1, y1) and (x2, y2) relative to positive y-axis
+    x_diff = x2 - x1
+    y_diff = y2 - y1
+    angle = 0
+
+    if x_diff >= 0 and y_diff <= 0: 
+      # If in upper right quodrant
+      y_diff = abs(y_diff)
+      angle = math.atan(x_diff / y_diff)
+    elif x_diff >= 0 and y_diff >= 0: 
+      # If in lower right quodrant
+      angle = math.atan(y_diff / x_diff) + math.pi/2
+    elif x_diff <= 0 and y_diff >= 0: 
+      # If in lower left quodrant
+      x_diff = abs(x_diff)
+      angle = math.atan(x_diff / y_diff) + math.pi
+    else:
+      # If in upper left quodrant
+      x_diff = abs(x_diff)
+      y_diff = abs(y_diff)
+      angle = math.atan(y_diff / x_diff) + (3/2 * math.pi)
+
+    return angle
+
+
+class Game:
+  # Class for the game, includes frame trigger, pause/resume functions and etc.
   def __init__(self):
     pass
 
 
-class Game: # Class for the game, includes frame trigger, pause/resume functions and etc.
-  def __init__(self):
-    pass
-
-
-class Application: # Class for the whole application, contains tkinter top window and etc.
+class Application: 
+  # Class for the whole application, contains tkinter top window and etc.
   def __init__(self):
     self.main_window_dimensions = {
       "x": 1440,
       "y": 900
     }
+    # Initialize the main window
     self.main_window = tk.Tk()
     self.main_window.title("SpaceShootaz")
     self.main_window.geometry(f"{self.main_window_dimensions.get('x')}x{self.main_window_dimensions.get('y')}")
@@ -26,7 +59,10 @@ class Application: # Class for the whole application, contains tkinter top windo
     self.main_window.mainloop()
 
 def main():
-  app = Application()
+  #app = Application()
+  utils = Utilities()
+  utils.resolve_angle(250, 250, 245, 245)
+
 
 if __name__ == "__main__":
   main()
